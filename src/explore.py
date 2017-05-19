ba9f0a0045925cd32fd0017bc435156c4f068055
AbdelRahman <eng.abdelrahman.gaber@gmail.com>
2017-05-19 21:37:30 +0200

    adding explore node

---
+++ src/explore.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# ROS imports
+import roslib; roslib.load_manifest('autonomous_explore_map_plan')
+import rospy
+import tf
+import math
+
+#ROS messages
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from autonomous_explore_map_plan.srv import GotoWaypoint, GotoWaypointResponse, FindPathToGoal, FindPathToGoalResponse, FindPathToGoalRequest
+
+#Numpy
+import numpy as np
+
+from MapProcessing import ProcessMap
+from controller_turtlebot import Controller
+
+if __name__ == '__main__':
+    rospy.init_node('explore_node', log_level=rospy.INFO)
+    rospy.loginfo("%s: starting turtlebot exploration", rospy.get_name())
+
+    controller = Controller()
+    pm = ProcessMap()
+    pm.ProcessProjectedMap()
+
+    rospy.spin()
